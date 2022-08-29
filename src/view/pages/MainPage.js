2f43c56d882a69f7047cb28960fe8a661720d23d
Uni <joseoyun12@gmail.com>
2022-08-29 21:56:58 +0900

    메인페이지 컴포넌트 적용

--- src/view/pages/MainPage.js
+++ src/view/pages/MainPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Footer from '../components/Main/Footer';
+import IntroBanner from '../components/Main/IntroBanner';
+import MainBanner from '../components/Main/MainBanner';
+import Navbar from '../components/Main/Navbar';
 
 const MainPage = () => {
-	return <div>메ddd인</div>;
+	return (
+		<div>
+			<Navbar />
+			<MainBanner />
+			<IntroBanner />
+			<MainBanner />
+			<Footer />
+		</div>
+	);
 };
 
 export default MainPage;
