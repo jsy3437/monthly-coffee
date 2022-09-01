af23cc0ec9fb784999a00d30a74a040c06e500b1
Uni <joseoyun12@gmail.com>
2022-09-01 12:44:11 +0900

    Styled-components 전역적용함

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
+import GlobalFonts from './styles/GlobalFonts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
+		<GlobalStyle />
+		<GlobalFonts />
 		<App />
 	</BrowserRouter>
 );
