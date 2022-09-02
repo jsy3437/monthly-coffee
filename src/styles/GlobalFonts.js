a253ab915f73e1127b70b3836aee06cc99d8a447
Uni <joseoyun12@gmail.com>
2022-09-02 14:18:53 +0900

    전역 폰트 선언함

--- src/styles/GlobalFonts.js
+++ src/styles/GlobalFonts.js
@@ -1,7 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 import Mon_Bold from '../fonts/Montserrat-Bold.woff';
+import KR_Bold from '../fonts/NotoSansKR-Bold.woff';
+import KR_Medium from '../fonts/NotoSansKR-Medium.woff';
 
 const GlobalFonts = createGlobalStyle`
-   
+    @font-face {
+        font-family: 'mon-b';
+        src: local('mon-b'), url(${Mon_Bold}) format('woff');
+    }
+    @font-face {
+        font-family: 'kr-b';
+        src: local('kr-b'), url(${KR_Bold}) format('woff');
+    }
+    @font-face {
+        font-family: 'kr-m';
+        src: local('kr-m'), url(${KR_Medium}) format('woff');
+    }   
+
 `;
 export default GlobalFonts;
